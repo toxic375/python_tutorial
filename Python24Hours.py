6368ff8d2de215ea7b31f2fdd23b9b3433facc33
toxic375 <41807264+toxic375@users.noreply.github.com>
2018-08-25 07:50:59 -0700

    Initial

---
+++ Python24Hours.py
@@ -0,0 +1,43 @@
+#  Python tutorial
+########
+
+    """
+    Variables and Data Types
+    --------------------------
+    """
+
+
+        """ Variable Types """
+
+        #Integer (int) == whole numbers (1, 3, -6, 1000)
+        #Float == decimal numbers (3.14, 1.5, -2.8)
+        #Long == very large numbers (100000000005)
+        #String == letters, numbers, spaces, and symbols (Hello, ^E, 42)
+        #List == group of items enclosed by brackets and separated by commas ([1, 2, 4], ["Nevada", "California"])
+        #Tuple == lits of items that cannot be changed enclosed by parentheses (("Jack Jones", "11051994", "Seattle, WA"))
+        #Dictionary == list of keys and values that are paired, enclosed by curly braces ({'apple':'red', 'sky':'blue', 'dirt':'brown'})
+
+
+        A = 5    # To set a variable use '='. In this example 'A' is defined as '5'.
+        
+        type(a)    # This will print out the type of value stored in 'A'. Since 'A=5' this would return as '<type 'int'>'.
+
+        # If we were to set 'A' as something new here on Line12 it would erase '5' and replace it with the new value.
+        B = A     # We can also set one variable equal to the value of another variable.
+
+
+        """ VARIABLE RULES """
+
+        #1 Names cannot start with a number.
+        #2 Names cannot be too long (depends on the memory of the computer).
+        #3 Names cannot contain special characters (an underscore '_' is okay, ex. '_Variable21').
+
+
+
+        """ VARIABLE GUIDELINES """
+
+        #Variable names should always make sense. Describe what they contain.
+        #If you have to use more than one word, separate words with an underscore (_).
+        #Variable names should be lowercase(ex. '_variable21'). EXCEPTION: If you want to imply the variable is a constant, use all uppercase.
+        #If you are going to use a single letter, don't use a lowercase L or an uppercase O as they can look like 1s and 0s in some fonts.
+
